ab1a3f1b1d229023b3877644c6803126eaa3fa42
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-08-28 21:17:02 +0800

    Adds more comment on FileSort.

--- app/src/main/java/com/example/interviewquestion/algo/FileSort.kt
+++ app/src/main/java/com/example/interviewquestion/algo/FileSort.kt
@@ -14,6 +14,14 @@ class Solution {
                 if (result < 0 || result > 0) break
             }
 
+            /* After segmenting original size would change, because of combing the numerics as a unit
+            * in that case the comparison would based the length before segmentation.
+            *
+            * For example:
+            * file1 and file00001 going changed to same segment size and every segment matches
+            * the same as well, in that case original size is going to be used for comparison,
+            * in this case file1 < file00001
+            * */
             if (result == 0) {
                 result = a.length - b.length
             }
@@ -26,29 +34,35 @@ class Solution {
         val result = mutableListOf<Int>()
         var digits = StringBuilder()
         var chars = StringBuilder()
-        var previousChar: Char? = null
+        var previousChar: Char? = null // Previous char used for detecting sequence change
         for (i in input.indices) {
             val ch = input[i]
-            if (ch.isDigit()) {
+            if (ch.isDigit()) { // Segment digits
                 digits.append(Character.digit(ch, 10))
 
-                if (previousChar != null && !(previousChar.isDigit())) {
+                /* When sequence change from letters to numbers, it adds gathered letters to
+                * the result list
+                * **/
+                if (isSequenceChangedToNumber(previousChar)) {
                     chars.forEach {
                         result.add(it.code)
                     }
                     chars = StringBuilder()
                 }
                 previousChar = ch
-            } else {
+            } else {  // Segment letters
                 chars.append(ch)
-
-                if (previousChar != null && previousChar.isDigit()) {
+                /* Symmetric operation, when sequence change from numbers to letter, it combine
+                 * gathered letters and adds to the result list
+                 * **/
+                if (isSequenceChangedToLetter(previousChar)) {
                     result.add(digits.toString().toInt())
                     digits = StringBuilder()
                 }
                 previousChar = ch
             }
 
+            // Adds last remaining numbers and letters
             if (i == input.length - 1) {
                 if (digits.isNotEmpty()) {
                     result.add(digits.toString().toInt())
@@ -63,6 +77,12 @@ class Solution {
         }
         return result
     }
+
+    private fun isSequenceChangedToLetter(previousChar: Char?) =
+        previousChar != null && previousChar.isDigit()
+
+    private fun isSequenceChangedToNumber(previousChar: Char?) =
+        previousChar != null && !(previousChar.isDigit())
 }
 
 fun main() {
